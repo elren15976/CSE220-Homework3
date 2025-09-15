4171393c42466b7004fc908fad2cf647182bfb96
Elijah Ren <elijah.ren@stonybrook.edu>
2025-09-15 15:01:14 +0000

    Finished float

--- src/float.c
+++ src/float.c
@@ -9,11 +9,11 @@ float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
     
     /* Start Coding Here */
 
-    
-    
-    
-    
-    
+    f = fraction;   //Start with putting in the fraction
+
+    f |= exponent << 23;    //Inclusive or the exponent, after left shifting it by 23 spaces
+
+    f |= sign_bit << 31;    //Inclsive or the sign bit, after left shifting it by 31 spaces
     
     /*-------------------*/
     
@@ -37,7 +37,16 @@ int main(int argc, char* argv[]){
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
 
-
+    printf("f = %g\n", construct_float_sf(0x00, 0x81, 0x300000));
+    printf("f = %g\n", construct_float_sf(0x01, 0x81, 0x300000));
+    printf("f = %g\n", construct_float_sf(0x00, 0x7F, 0x200000));
+    printf("f = %g\n", construct_float_sf(0x01, 0x7F, 0x200000));
+    printf("f = %g\n", construct_float_sf(0x00, 0x76, 0x299B6F));
+    printf("f = %g\n", construct_float_sf(0x01, 0x76, 0x299B6F));
+    printf("f = %g\n", construct_float_sf(0x00, 0x89, 0xABCDEF));
+    printf("f = %g\n", construct_float_sf(0x01, 0x89, 0xABCDEF));
+    printf("f = %g\n", construct_float_sf(0x00, 0x90, 0x7973C0));
+    printf("f = %g\n", construct_float_sf(0x01, 0x90, 0x7973C0));
 
 	
 	/** ---------------------- **/
